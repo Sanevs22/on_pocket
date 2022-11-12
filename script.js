693838887c2e38ff8f36416796a3fd8a866dffc3
Alex Evs <sanevs@bk.ru>
2022-11-12 23:13:26 +0300

    add safe

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-document.getElementById('plan').value = '150';
-var res = document.getElementById('res');
-res.textContent = '150';
+var p = document.getElementById('plan'); 
+var t = document.getElementById('trat');
+var ost = document.getElementById('res');
+
 var Date = new Date;
 var month = Date.getMonth() + 1;
-document.getElementById('date').textContent = Date.getDate() +'.' + month +'.' + Date.getFullYear();
+var time = Date.getDate() +'.' + month +'.' + Date.getFullYear();
+document.getElementById('date').textContent = time;
 
+if(localStorage.getItem('plan')) {
+    p.value = Number(localStorage.getItem('plan'));
+} else {
+    p.value = 100;
+}
 
+if(localStorage.getItem('trat')) {
+    t.value = Number(localStorage.getItem('trat'));
+} else {
+    t.value = 0;
+}
+
+if(localStorage.getItem('time') !== time) {
+    p.value = Number(localStorage.getItem('ost')) + 100;
+    t.value = 0;
+} 
+
+ost.textContent = p.value-t.value;
 
 document.querySelector('button').onclick = function() {
-    var p = document.getElementById('plan').value;
-    var t = document.getElementById('trat').value;
-    res.textContent = p - t;
+    ost.textContent = p.value-t.value;
+    localStorage.setItem('ost', ost.textContent);
+    localStorage.setItem('time', time);
+    localStorage.setItem('plan', p.value);
+    localStorage.setItem('trat', t.value);
 }
